40eea5cee3b2a2944213ade113dcd0f83fb1216c
Anish <xevenbiswas@gmail.com>
2023-11-11 19:28:00 +0530

    desgnid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-// App.jsx
 import { useState, useEffect } from "react";
 import { exchangeRatesData, fetchExchangeRates } from "./exchangeRatesData";
 import currencyCodes from "./currencyCodes";
+import "./App.css";
+import hero from"./hero.svg";
 
 const Home = () => {
+  const currentTime = new Date();
+  const cDateTime = `${currentTime.getDate()}-${
+    currentTime.getMonth() + 1
+  }-${currentTime.getFullYear()} ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+
   const [baseAmount, setBaseAmount] = useState("1");
   const [convertedAmount, setConvertedAmount] = useState("1");
   const [baseCurrency, setBaseCurrency] = useState("USD");
@@ -31,58 +37,68 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Currency Converter</h1>
-      <div>
-        <label htmlFor="baseAmount">Base Amount:</label>
-        <input
-          type="number"
-          id="baseAmount"
-          name="baseAmount"
-          value={baseAmount}
-          onChange={(e) => setBaseAmount(e.target.value)}
-        />
-        <select
-          name="baseCurrency"
-          id="bcopt"
-          value={baseCurrency}
-          onChange={(e) => setBaseCurrency(e.target.value)}
-        >
-          {Object.keys(currencyCodes).map((code) => (
-            <option key={code} value={code}>
-              {code} - {currencyCodes[code]}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="convertedAmount">Converted Amount:</label>
-        <input
-          type="text"
-          id="convertedAmount"
-          name="convertedAmount"
-          value={convertedAmount}
-          readOnly
-        />
-        <select
-          name="targetCurrency"
-          id="ccopt"
-          value={targetCurrency}
-          onChange={(e) => setTargetCurrency(e.target.value)}
-        >
-          {Object.keys(currencyCodes).map((code) => (
-            <option key={code} value={code}>
-              {code} - {currencyCodes[code]}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
+    <>
+      <div className="container">
+        <h1>Currency Converter</h1>
+        <img src={hero} alt="currency"/>
+        <div className="inputs">
+          <div className="box">
+            <label htmlFor="baseAmount">Base Amount:</label>
+            <input
+              type="number"
+              id="baseAmount"
+              name="baseAmount"
+              value={baseAmount}
+              onChange={(e) => setBaseAmount(e.target.value)}
+            />
+            <select
+              name="baseCurrency"
+              id="bcopt"
+              value={baseCurrency}
+              onChange={(e) => setBaseCurrency(e.target.value)}
+            >
+              {Object.keys(currencyCodes).map((code) => (
+                <option key={code} value={code}>
+                  {code} - {currencyCodes[code]}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="box">
+            <label htmlFor="convertedAmount">Converted Amount:</label>
+            <input
+              type="text"
+              id="convertedAmount"
+              name="convertedAmount"
+              value={convertedAmount}
+              readOnly
+            />
+            <select
+              name="targetCurrency"
+              id="ccopt"
+              value={targetCurrency}
+              onChange={(e) => setTargetCurrency(e.target.value)}
+            >
+              {Object.keys(currencyCodes).map((code) => (
+                <option key={code} value={code}>
+                  {code} - {currencyCodes[code]}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
         <button onClick={convertCurrency}>Convert</button>
       </div>
-    </div>
+      <footer>
+        <p>
+          © Created by{" "}
+          <a href="https://bento.me/anish7" target="_blank" rel="noreferrer">
+            Anish
+          </a>
+          . Data is last updated at {cDateTime}
+        </p>
+      </footer>
+    </>
   );
 };
 
--- src/exchangeRatesData.js
+++ src/exchangeRatesData.js
@@ -4,7 +4,7 @@ let exchangeRatesData = {
     data: {},
 };
 const apikey = "fca_live_bvayG1a9oqSIgB5qdTe4xJf81jtBm7hld4EZX8Yb";
-const url = `https://api.freecurrencyapi.com/v1/latest?apikey=${apikey}`;
+const url = `https://api.afreecurrencyapi.com/v1/latest?apikey=${apikey}`;
 
 const fetchExchangeRates = async () => {
     try {
